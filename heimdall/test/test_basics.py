6bfd8887dc75031b7f09f59ff906c457770866dc
PouncySilverkitten <struan@duncan-wilson.co.uk>
2018-03-18 11:33:41 +0000

    More tests

--- heimdall/test/test_basics.py
+++ heimdall/test/test_basics.py
@@ -23,6 +23,8 @@ class TestBasics(unittest.TestCase):
         assert self.heimdall.date_from_timestamp(1521314568) == "2018-03-17"
         assert self.heimdall.date_from_timestamp(1552850568) == "2019-03-17"
 
+    
+
 def main():
     unittest.main()
 
